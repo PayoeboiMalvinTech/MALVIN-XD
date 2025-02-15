33b4bdf5cd4c6f3887ea99dc3757d82988fd2383
PayoeboiMalvinTech <malvink002@gmail.com>
2025-02-15 10:23:52 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
+SESSION_ID: process.env.SESSION_ID || "MALVIN-XD~hs4G1ZCZ#3v7EQC-ic9BIGw4daqNQ12O4sYlplZ6ZXfRlAHLdS_U",
 // add your Session Id 
 
 PREFIX: process.env.PREFIX || ".",
